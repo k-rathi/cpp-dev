c74aa17b0c2ab3efbb351f74371cf41b477acf2c
k-rathi <kunalrathi1994@gmail.com>
2017-07-13 19:11:51 -0700

    Initial Commit

---
+++ c_plus_plus_learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_plus_plus_learning
+//
+//  Created by Kunal Rathi on 7/13/17.
+//  Copyright © 2017 Kunal Rathi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
